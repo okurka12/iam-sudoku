16d8f9432eb38920ff66f64d8dc4110b3a12a9f5
Vit Pavlik <pavlikvitek13@gmail.com>
2024-05-30 08:46:56 +0200

    make view.py check the model

--- view.py
+++ view.py
@@ -9,6 +9,12 @@
 # a view program accompanying sudoku.py
 # used python version: Python 3.12.3 (GCC 13.1.0) on Debian 12.5
 #
+# also checks if the model is valid
+#
+# expects SAT output from minisat
+# tested with Debian package minisat 1:2.2.1-5+b3 (bookworm)
+#
+from itertools import combinations
 from typing import List
 import sys
 
@@ -35,14 +41,18 @@ class Position:
         y = num // (9 * 9)
         x = (num - y * 9 * 9) // 9
         value = num % 9
+        segment = 3 * (y // 3) + (x // 3)
         # print(f"{num} {x=} {y=} {value=}")
         self.x = x
         self.y = y
         self.value = value
         self.num = num
+        self.segment = segment
 
 
 input_text = sys.stdin.read()
+
+assert "unsat" not in input_text.lower()
 
 positions: List[Position] = []
 
@@ -57,4 +67,24 @@ assert len(positions) == 81, "there must be 81 true variables..."
 for pos in positions:
     template = template.replace("x", str(pos.value), 1)
 
+# perform checks
+error_found = False
+for p, q in combinations(positions, 2):
+    if p.x == q.x and p.value == q.value:
+        error_found = True
+        print(f"conflicting numbers {p.value} in column {p.x + 1}")
+    if p.y == q.y and p.value == q.value:
+        error_found = True
+        print(f"conflicting numbers {p.value} in row {p.y + 1}")
+    if p.segment == q.segment and p.value == q.value:
+        error_found = True
+        print(f"conflicting numbers {p.value} in segment {p.segment + 1}")
+if error_found:
+    print("errors were found")
+else:
+    print("no errors found")
+
+
 print(template)
+if error_found:
+    print("errors were found")
